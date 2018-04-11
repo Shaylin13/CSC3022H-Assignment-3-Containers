ab257a3e35afc149a0437d883825d7d283f5ddfc
Shaylin <antelopes13@gmail.com>
2018-04-11 13:27:26 +0200

    same functionality while using shared_ptr instead of raw pointer

--- HuffmanTree.h
+++ HuffmanTree.h
@@ -10,27 +10,34 @@ namespace PDYSHA009
 {
     class HuffmanTree
     {
-    public:
+        public:
     
         //pointer to store root node of built tree when this is lost whole tree should destruct
         //important to tell class of HuffmanNode
         std::shared_ptr<class HuffmanNode> root;
         
         std::unordered_map<char, int> FrequencyTable;
-        
+        std::map<char,std::string> CodeTable;
+
         HuffmanTree();//default constructor
         ~HuffmanTree();//destructor
         
+        
         void buildFrequencyTable(std::string inputFile);
+        void generateTree(std::unordered_map<char,int> u_map);
+        
         
     };
     
     class HuffmanNode
     {
-    public:
+        public:
         
         char data;
         int freq;
+        
+        ///*std::shared_ptr<HuffmanNode>*/HuffmanNode *left;
+        ///*std::shared_ptr<HuffmanNode>*/HuffmanNode *right;
         
         std::shared_ptr<HuffmanNode> left;
         std::shared_ptr<HuffmanNode> right;
@@ -38,6 +45,7 @@ namespace PDYSHA009
         bool internalNode;
         
         HuffmanNode(char data, int freq);//default constructor
+        //HuffmanNode(char data, int freq, shared_ptr<HuffmanNode> l, shared_ptr<HuffmanNode> r);
         ~HuffmanNode();//destructor
         
     };
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <unordered_map>
 #include <bits/stdc++.h>
+#include <queue>
 
 
 using namespace std;
@@ -20,6 +21,7 @@ HuffmanTree::~HuffmanTree()//default destructor
     this->root = nullptr;
 }
 
+//---------------------------------------------------------------------------------------{
 void HuffmanTree::buildFrequencyTable(string inputFile)
 {
     
@@ -47,13 +49,11 @@ void HuffmanTree::buildFrequencyTable(string inputFile)
             while(line[i]!='\0')
             {
                 it=m.find(line[i]);
-                if(it==m.end()/*&&line[i]!='\n'*/)
+                if(it==m.end())
                 {
                     int c=count(line.begin(),line.end(),line[i]);//returns the occurances of line[i] in the range
                     m.insert(pair<char,int>(line[i],c));//adds the key and count of occurance to the unordered_map
                 }
-                /*if(line[i]=='\n')//this is if you want to represent "\n"/space with @, not neccessary
-                    m['@']+=1; //for \n*/
                 i++;
             }
         
@@ -61,16 +61,100 @@ void HuffmanTree::buildFrequencyTable(string inputFile)
         HuffmanTree::FrequencyTable = m;//assigning value to the FrequencyTable member variable
     }
 }
+//---------------------------------------------------------------------------------------}
+
+//======================================================================================={
+
+struct compare {
+ 
+        bool operator()(const HuffmanNode& l, const HuffmanNode& r)
+        {
+            return (l.freq > r.freq);
+        }
+    };
+    
+void getCodes(const HuffmanNode& root, string str, class HuffmanTree *h)
+{
+    
+    if(!root.left && !root.right)
+    {
+        h->CodeTable.insert(pair<char,string>(root.data,str));
+    }
+ 
+    else
+    {    
+        if(root.left)
+            {getCodes(*root.left, str + "0",h);}
+        if(root.right)
+        {getCodes(*root.right, str + "1",h);}
+    }
+    
+}//end of printCodes()
+
+void HuffmanTree::generateTree(unordered_map<char,int> u_map)
+{
+    cout<<"generateTree"<<endl;
+    
+    class HuffmamnNode;
+    /*shared_ptr<HuffmanNode> left = nullptr;
+    shared_ptr<HuffmanNode> right = nullptr;
+    shared_ptr<HuffmanNode> top;*/
+ 
+    // Create a min heap & inserts all characters of data[]
+    priority_queue<HuffmanNode, vector<HuffmanNode>, compare> minHeap;
+ 
+        unordered_map<char,int>::iterator it;
+        for(it=u_map.begin();it!=u_map.end();++it)
+            {
+                HuffmanNode temp (it->first, it->second);
+                minHeap.push(temp);//pushing values from map onto priority queue
+            }
+            cout<<"Priority queue size "<<minHeap.size()<<endl;
+            
+        while(minHeap.size() >1)
+        {
+            HuffmanNode left = minHeap.top();
+            minHeap.pop();
+            
+            HuffmanNode right = minHeap.top();
+            minHeap.pop();
+            
+            HuffmanNode top('$',(left.freq + right.freq));
+            //top.left = shared_ptr<HuffmanNode>(new HuffmanNode(left.data,left.freq));
+            //top.right = shared_ptr<HuffmanNode>(new HuffmanNode(right.data,right.freq));
+            
+            top.left = shared_ptr<HuffmanNode>(new HuffmanNode(left));
+            top.right = shared_ptr<HuffmanNode>(new HuffmanNode(right));
+            
+            minHeap.push(top);
+        }
+        
+        HuffmanNode root = minHeap.top();
+        this->root = shared_ptr<HuffmanNode>(new HuffmanNode(root));
+ 
+    
+    // add huffman codes to CodeTable using
+    // the Huffman tree built above
+    //rootNode = minHeap.top();
+    //cout<<"Root Node"<<rootNode->data<<endl;
+    getCodes(root, "",this);
+    
+}
+
+//=======================================================================================}
 
 HuffmanNode::HuffmanNode(char data, int freq)//default constructor
 {
-    cout<<"HuffmanNode() default constructor"<<endl;
+    //cout<<"HuffmanNode() default constructor"<<endl;
     this->data = data;
     this->freq = freq;
+    this->left=nullptr;
+    this->right=nullptr;
 }
+
 HuffmanNode::~HuffmanNode()//default destructor
 {
-    cout<<"HuffmanNode() destructor "<<this->data<<endl;
+    //cout<<"HuffmanNode() destructor "<<this->data<<endl;
 }
 
 
@@ -80,16 +164,17 @@ int main(int argc, char* argv[])
 //test block
 //===================================================
 cout<<"\n"<<endl;
-shared_ptr<HuffmanNode> test(new HuffmanNode('a',0));
+//shared_ptr<HuffmanNode> test(new HuffmanNode('a',0));
 
 HuffmanTree newHuffmanTree;
-newHuffmanTree.root = test;
+//newHuffmanTree.root = test;
 
 //cout<<newHuffmanTree.root->data<<endl;
 newHuffmanTree.buildFrequencyTable(argv[1]);
+newHuffmanTree.HuffmanTree::generateTree(newHuffmanTree.HuffmanTree::FrequencyTable);
 //===================================================
 
-//test that FrequencyTable is correct================
+//test that FrequencyTable is correct
     cout<<"Frequency Table:\n"<<endl;
     cout<<"char : Frequency\n"<<endl;
         unordered_map<char,int> u_map = newHuffmanTree.HuffmanTree::FrequencyTable;
@@ -102,6 +187,19 @@ newHuffmanTree.buildFrequencyTable(argv[1]);
                     cout<<"\\n"<<":"<<it->second<<endl;
             }
             cout<<"\n"<<endl;
+            
+//test that CodeTable is correct
+    cout<<"char : Huffman Code\n"<<endl;
+        map<char,string> n_map = newHuffmanTree.HuffmanTree::CodeTable;
+        map<char,string>::iterator it2;
+        for(it2=n_map.begin();it2!=n_map.end();++it2)
+            {
+                if((it2->first)!='\n')
+                    cout<<it2->first<<" :"<<it2->second<<endl;
+                else
+                    cout<<"\\n"<<":"<<it2->second<<endl;
+            }
+            cout<<"\n"<<endl;          
 //===================================================
 
 return 0;
